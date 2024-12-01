5ce9b9f685730f7ee4db7d8fb3085a9bc81ecef4
Ankit Kumar <abhishekankit128@gmail.com>
2024-12-01 23:16:57 +0530

    s

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // API Routes
 app.use("/api/hotels", hotelRoutes);
